1011179779d546674871b6f83ae6e771abed7656
Henryk Ratajczak <henryk.ratajczak@allegro.pl>
2020-04-29 12:32:38 +0200

    Revert changes to bitbucket emoji from 7.0.0 that don't work for us and I don't like.

--- lib/danger/helpers/emoji_mapper.rb
+++ lib/danger/helpers/emoji_mapper.rb
@@ -8,10 +8,10 @@ module Danger
         "white_check_mark" => "✅"
       },
       "bitbucket_server" => {
-        "no_entry_sign"    => ":no_entry_sign:",
-        "warning"          => ":warning:",
-        "book"             => ":book:",
-        "white_check_mark" => ":white_check_mark:"
+        "no_entry_sign"    => "❌",
+        "warning"          => "⚠️",
+        "book"             => "🔮",
+        "white_check_mark" => "✅"
       }
     }.freeze
 
